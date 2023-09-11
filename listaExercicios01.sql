93551115f1abb8611b8f1b07bfaea1968cbc8e0b
nycollas177 <102814318+nycollas177@users.noreply.github.com>
2023-09-11 16:57:07 -0300

    Update listaExercicios01.sql
    commit 20

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -105,6 +105,13 @@ FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
 GROUP BY alunos.id;
 
+ exercício 20
+SELECT produto, COUNT() AS quantidade_de_vendas
+FROM vendas
+GROUP BY produto
+ORDER BY quantidade_de_vendas DESC
+LIMIT 1;
+
 
 
 
