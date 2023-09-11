b3f15d602920d3d2eb560282e2545d3c7c9db608
nycollas177 <102814318+nycollas177@users.noreply.github.com>
2023-09-11 16:38:05 -0300

    Update listaExercicios01.sql
    commit 3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -4,3 +4,8 @@ exercício 2
 select NOME from autores
 WHERE nascimento < '1900-01-01';
 
+exercício 3
+SELECT titulo 
+from livros 
+inner join autores on autor_id=autores.id
+where NOME = 'J.K. Rowling';
