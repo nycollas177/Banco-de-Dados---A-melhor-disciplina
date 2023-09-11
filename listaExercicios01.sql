89527c74aa0e556be2639a83bcb559e4d9b98a55
nycollas177 <102814318+nycollas177@users.noreply.github.com>
2023-09-11 16:47:57 -0300

    Update listaExercicios01.sql
    commit 11

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -50,3 +50,9 @@ LEFT JOIN livros ON autores.id = livros.autor_id
 GROUP BY autores.id
 HAVING COUNT(livros.id) > 2;
 
+exercício 11
+SELECT livros.titulo, autores.nome AS autor
+FROM livros
+JOIN autores ON livros.autor_id = autores.id;
+
+
